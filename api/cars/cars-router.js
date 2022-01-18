a03ef235018583115a5d45376d3798174956aad8
FftyShadesofCode <philiprmcdavid@gmail.com>
2022-01-18 17:12:01 -0500

    aayooooo

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,34 @@
-// DO YOUR MAGIC
+const express = require('express')
+const Car = require('./cars-model')
+const {
+    checkCarId,
+    checkCarPayload,
+    checkVinNumberValid,
+    checkVinNumberUnique
+} = require('./cars-middleware')
+
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Car.getAll()
+        res.json(cars)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const car = await Car.getById(req.params.id)
+        res.json(car)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    res.json('posting mechanical information')
+})
+
+module.exports = router

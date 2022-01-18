3fe35eea64ffc864d932fa8d40f562b922adcc0e
FftyShadesofCode <philiprmcdavid@gmail.com>
2022-01-18 18:15:01 -0500

    aayooooo

--- api/server.js
+++ api/server.js
@@ -1,18 +1,12 @@
 const express = require("express")
 const carsRouter = require('./cars/cars-router')
-
 const server = express()
 
 server.use(express.json())
-
 server.use('/api/cars', carsRouter)
 
-server.use('*', (req, res, next) => {
-    next({status: 404, message: 'not found'})
-})
-
-server.use((err, req, res, next) => { // eslint-disable-line
-    res.status(err.status || 500).json({
+server.get('/', (err, req, res) => {
+    res.send(err.status || 500).json({
         message: err.json
     })
 })
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -4,18 +4,19 @@ exports.up = function (knex) {
 
       tbl.string('vin', 17).notNullable().unique()
 
-      tbl.string('make', 150).notNullable()
+      tbl.string('make', 20).notNullable()
 
-      tbl.string('model', 150).notNullable()
+      tbl.string('model', 20).notNullable()
 
       tbl.integer('mileage').unsigned().notNullable()
 
-      tbl.string('title', 150).defaultTo('not specified')
+      tbl.string('title').defaultTo('not specified')
 
-      tbl.string('transmission', 150).defaultTo('not specified')
+      tbl.string('transmission').defaultTo('not specified')
   })
-};
+}
 
-exports.down = function (knex) {
-    return knex.schema.dropTableIfExists('cars')
-};
+exports.down = async function (knex) {
+    // DO YOUR MAGIC
+    await knex.schema.dropTableIfExists('cars')
+}

fec4cf03236140500b37e6bb5c41ffae326976bb
FftyShadesofCode <philiprmcdavid@gmail.com>
2022-01-18 16:07:24 -0500

    making headway

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,21 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+      tbl.increments()
+
+      tbl.string('vin', 17).notNullable().unique()
+
+      tbl.string('make', 150).notNullable()
+
+      tbl.string('model', 150).notNullable()
+
+      tbl.numeric('mileage').unsigned().notNullable()
+
+      tbl.string('title', 150).defaultTo('not specified')
+
+      tbl.string('transmission', 150).defaultTo('not specified')
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,39 @@
 // STRETCH
+const cars = [
+    {
+        vin: '11111111111111111',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 15382,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '11111111111111111',
+        make: 'dodge',
+        model: 'stratus',
+        mileage: 843,
+        title: 'salvage',
+        transmission: 'automatic'
+    },
+    {
+        vin: '11111111111111111',
+        make: 'dodge',
+        model: 'ram',
+        mileage: 312,
+        transmission: 'automatic'
+    },
+]
+
+// exports.seed = function (knex) {
+//     return knex('cars')
+//         .truncate().then(() => {
+//             return knex('cars').insert(cars)
+//         })
+//
+// }
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate().then()
+    await knex('cars').insert(cars)
+}
